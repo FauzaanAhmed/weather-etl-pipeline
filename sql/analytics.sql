44cf8e9279cd06eb1e0dea29f354b7e30dc6eb06
Fauzaan Mohammad <fauzaanahmed@gmail.com>
2026-05-19 15:26:35 -0400

    analytics sql

---
+++ sql/analytics.sql
@@ -0,0 +1,19 @@
+-- Example analytics queries for the weather table
+
+SELECT
+    date,
+    avg(air_temperature_avg) AS avg_temp,
+    avg(wind_speed_avg) AS avg_wind
+FROM weather
+WHERE date >= current_date - interval '30 days'
+GROUP BY date
+ORDER BY date;
+
+-- Hottest stations on a given day
+SELECT
+    station_id,
+    air_temperature_max
+FROM weather
+WHERE date = current_date - 1
+ORDER BY air_temperature_max DESC
+LIMIT 20;
